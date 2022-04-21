f6e359b56505fdb2ac6d5696b7d4d62b843ab9de
Viktor Kravchuk <viktorkrav4uk87@gmail.com>
2022-04-21 15:52:27 +0300

    Refactor ideas

--- src/frontend/src/pages/Home/components/Ideas/Ideas.view.tsx
+++ src/frontend/src/pages/Home/components/Ideas/Ideas.view.tsx
@@ -1,23 +1,9 @@
 import React, { useEffect } from 'react'
-import axios from 'axios'
-import { Link } from 'react-router-dom'
-import { Page } from 'styles/components'
 
 // prettier-ignore
 import { IdeasSection } from './Ideas.style'
 
 export const IdeasView = () => {
-  const handleFetchData = async () => {
-    try {
-      const data = await axios.get('https://medium.com/feed/@Mavryk_Finance')
-      console.log('%c ||||| data', 'color:yellowgreen', data)
-    } catch (error) {
-      console.log('%c ||||| error', 'color:yellowgreen', error)
-    }
-  }
-
-  useEffect(() => {
-    //handleFetchData()
-  }, [])
+  // will be Ideas
   return null
 }
