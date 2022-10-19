7669a2e0967df76145871c778afdd8eb3db95d05
Kucher Maxym <max.kucher98@gmail.com>
2022-10-19 12:57:49 +0300

    fixed footer text size

--- src/frontend/src/app/App.components/Footer/Footer.style.tsx
+++ src/frontend/src/app/App.components/Footer/Footer.style.tsx
@@ -24,6 +24,7 @@ export const FooterStyled = styled.footer<{ theme: MavrykTheme }>`
     div {
       > div {
         color: ${({ theme }) => theme.footerText};
+        font-size: 16px;
       }
     }
 
