db069ddd2105a55840316b377f6bcfb1b0741894
Kucher Maxym <max.kucher98@gmail.com>
2022-10-26 18:32:43 +0300

    fixed wallet copy

--- src/frontend/src/app/App.components/ConnectWallet/ConnectWallet.view.tsx
+++ src/frontend/src/app/App.components/ConnectWallet/ConnectWallet.view.tsx
@@ -148,7 +148,7 @@ export const ConnectedWalletBlock = ({
           <div className="details-wallet ">
             <Icon id="wallet" className="wallet hover" />
             <var className="wallet-details-address hover">
-              <TzAddress tzAddress={accountPkh} hasIcon type={BLUE} shouldCopy={false} />
+              <TzAddress tzAddress={accountPkh} hasIcon type={BLUE} />
             </var>
           </div>
 
