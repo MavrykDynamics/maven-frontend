d881aebb40a7a37e2738a04d050ff9cb6ec89c78
Sam Morrow <smorrow32@gmail.com>
2022-12-05 17:22:52 +0200

    testing cloudflare update

--- src/frontend/src/pages/Litepaper/Litepaper.controller.tsx
+++ src/frontend/src/pages/Litepaper/Litepaper.controller.tsx
@@ -3,5 +3,6 @@ import * as React from 'react'
 import { LitepaperView } from './Litepaper.view'
 
 export const Litepaper = () => {
+  console.log('test cloudflare update')
   return <LitepaperView />
 }
