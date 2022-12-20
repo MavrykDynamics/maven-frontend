117a1bce25d30fd6404b325070127500f1ac3a42
Evgeniy Polishchuk <68184643+CasualJackie@users.noreply.github.com>
2022-12-20 17:52:52 +0200

    added bg images

--- src/frontend/src/pages/Bakery/Bakery.controller.tsx
+++ src/frontend/src/pages/Bakery/Bakery.controller.tsx
@@ -1,14 +1,67 @@
-import React from "react";
+import { useCallback, useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { SWRConfig } from 'swr'
 
 // components
+import { MenuTopBar } from 'app/App.components/Menu/MenuTopBar.controller'
+import { Footer } from 'app/App.components/Footer/Footer.controller'
+import { PopupChangeNodeView } from 'app/App.components/SettingsPopup/SettingsPopup.view'
+import { PopupChangeNode } from 'app/App.components/SettingsPopup/SettingsPopup.controller'
+import { connect } from 'redux/actions/connectWallet.actions'
+
+// helpers
+import { dexGqlFetcher } from '../../gql/gql.helpers'
+import { SPACE_THEME, toggleRPCNodePopup } from 'redux/actions/preferences.action'
+import { getItemFromStorage, setItemInStorage } from 'utils/utils'
+
+// types
+import { State } from 'utils/interfaces'
 
 // styles
 import { BakeryStyled } from "./Bakery.style";
 
 export function Bakery () {
+  const dispatch = useDispatch()
+  const { changeNodePopupOpen } = useSelector((state: State) => state.preferences)
+  const [isIOS, setIsIOS] = useState(true)
+
+  const openChangeNodePopup = useCallback(() => dispatch(toggleRPCNodePopup(true)), [])
+  const closeModalHandler = useCallback(() => dispatch(toggleRPCNodePopup(false)), [])
+
+  useEffect(() => {
+    if (
+      localStorage.getItem('beacon:active-account') &&
+      localStorage.getItem('beacon:active-account') !== 'undefined'
+    ) {
+      dispatch(connect())
+    }
+
+    if (!getItemFromStorage('theme')) {
+      setItemInStorage('theme', SPACE_THEME)
+    }
+
+    setIsIOS(
+      ['iPad Simulator', 'iPhone Simulator', 'iPod Simulator', 'iPad', 'iPhone', 'iPod'].includes(navigator.platform),
+    )
+  }, [])
+  
+
+  if (isIOS && changeNodePopupOpen) {
+    return <PopupChangeNodeView closeModal={closeModalHandler} />
+  }
   return (
-    <BakeryStyled>
-      
-    </BakeryStyled>
+    <SWRConfig
+      value={{
+        refreshInterval: 5000,
+        fetcher: dexGqlFetcher,
+      }}
+      >
+        <MenuTopBar openChangeNodePopupHandler={openChangeNodePopup} />
+        <PopupChangeNode isModalOpened={!isIOS && changeNodePopupOpen} closeModal={closeModalHandler} />
+        <BakeryStyled>
+            
+        </BakeryStyled>
+        <Footer />
+    </SWRConfig>
   )
 }
--- src/frontend/src/pages/Bakery/Bakery.style.tsx
+++ src/frontend/src/pages/Bakery/Bakery.style.tsx
@@ -1,5 +1,16 @@
 import styled from "styled-components";
 
 export const BakeryStyled = styled.div`
-  
+  background-image: ${({ theme }) =>
+  `url(${theme.theme === 'light' ? 'images/bakery/bakery-light-desktop-bg.svg' : 'images/bakery/bakery-dark-desktop-bg.svg'})`};
+
+  @media screen and (max-width: 769px) {
+    background-image: ${({ theme }) =>
+      `url(${theme.theme === 'light' ? 'images/lb/lb-light-mob-bg.svg' : 'images/lb/lb-dark-mob-bg.svg'})`};
+  }
+height: 1500px;
+  background-repeat: no-repeat;
+  background-size: cover;
+  position: relative;
+  padding-bottom: 100px;
 `
