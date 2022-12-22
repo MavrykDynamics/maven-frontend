dd0522286cbaa92fdb172e9fc6f1b00c4b0db7e8
Evgeniy Polishchuk <68184643+CasualJackie@users.noreply.github.com>
2022-12-22 19:11:28 +0200

    moved popup to App component from bakery

--- src/frontend/src/app/App.controller.tsx
+++ src/frontend/src/app/App.controller.tsx
@@ -1,16 +1,28 @@
+import { useCallback, useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { AnyAction } from 'redux'
+import { ThunkDispatch } from 'redux-thunk'
+
+// components
+import { PopupChangeNodeView } from 'app/App.components/SettingsPopup/SettingsPopup.view'
+import { PopupChangeNode } from 'app/App.components/SettingsPopup/SettingsPopup.controller'
+import { connect } from 'redux/actions/connectWallet.actions'
+import { Toaster } from './App.components/Toaster/Toaster.controller'
+import Loader from './App.components/Loader/Loader.view'
 import { Home } from 'pages/Home/Home.controller'
 import LiquidityBaking from 'pages/LiquidityBaking/LiquidityBaking.controller'
 import { Bakery } from 'pages/Bakery/Bakery.controller'
 import { Litepaper } from 'pages/Litepaper/Litepaper.controller'
 import { Privacy } from 'pages/Privacy/Privacy.controller'
-import { configureStore } from 'redux/storeConfigurator'
-import { State } from 'utils/interfaces'
-import { Toaster } from './App.components/Toaster/Toaster.controller'
-import Loader from './App.components/Loader/Loader.view'
 
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import { AnyAction } from 'redux'
-import { ThunkDispatch } from 'redux-thunk'
+// helpers
+import { SPACE_THEME, toggleRPCNodePopup } from 'redux/actions/preferences.action'
+import { getItemFromStorage, setItemInStorage } from 'utils/utils'
+import { configureStore } from 'redux/storeConfigurator'
+
+// types
+import { State } from 'utils/interfaces'
 
 export const store = configureStore({})
 
@@ -18,8 +30,37 @@ export type AppDispatch = ThunkDispatch<State, unknown, AnyAction>
 export type GetState = typeof store.getState
 
 export const App = () => {
+  const dispatch = useDispatch()
+  const { changeNodePopupOpen } = useSelector((state: State) => state.preferences)
+  const [isIOS, setIsIOS] = useState(true)
+
+  const openChangeNodePopup = useCallback(() => dispatch(toggleRPCNodePopup(true)), [])
+  const closeModalHandler = useCallback(() => dispatch(toggleRPCNodePopup(false)), [])
+
+  useEffect(() => {
+    if (
+      localStorage.getItem('beacon:active-account') &&
+      localStorage.getItem('beacon:active-account') !== 'undefined'
+    ) {
+      dispatch(connect())
+    }
+
+    if (!getItemFromStorage('theme')) {
+      setItemInStorage('theme', SPACE_THEME)
+    }
+
+    setIsIOS(
+      ['iPad Simulator', 'iPhone Simulator', 'iPod Simulator', 'iPad', 'iPhone', 'iPod'].includes(navigator.platform),
+    )
+  }, [])
+  
+
+  if (isIOS && changeNodePopupOpen) {
+    return <PopupChangeNodeView closeModal={closeModalHandler} />
+  }
   return (
     <>
+      <PopupChangeNode isModalOpened={!isIOS && changeNodePopupOpen} closeModal={closeModalHandler} />
       <Loader />
       <Router>
         {/* <Popup /> */}
@@ -48,7 +89,7 @@ export const App = () => {
           <Route
             exact
             path="/bakery"
-            component={Bakery}
+            component={() => <Bakery openChangeNodePopup={openChangeNodePopup}/>}
           />
         </Switch>
         <Toaster />
--- src/frontend/src/pages/Bakery/Bakery.controller.tsx
+++ src/frontend/src/pages/Bakery/Bakery.controller.tsx
@@ -1,66 +1,20 @@
-import { useCallback, useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { SWRConfig } from 'swr'
-
 // components
 import { BakeryView } from './Bakery.view'
 import { MenuTopBar } from 'app/App.components/Menu/MenuTopBar.controller'
-import { PopupChangeNodeView } from 'app/App.components/SettingsPopup/SettingsPopup.view'
-import { PopupChangeNode } from 'app/App.components/SettingsPopup/SettingsPopup.controller'
-import { connect } from 'redux/actions/connectWallet.actions'
-
-// helpers
-import { dexGqlFetcher } from '../../gql/gql.helpers'
-import { SPACE_THEME, toggleRPCNodePopup } from 'redux/actions/preferences.action'
-import { getItemFromStorage, setItemInStorage } from 'utils/utils'
-
-// types
-import { State } from 'utils/interfaces'
 
 // styles
 import { FooterStyled } from "./Bakery.style";
 
-export function Bakery () {
-  const dispatch = useDispatch()
-  const { changeNodePopupOpen } = useSelector((state: State) => state.preferences)
-  const [isIOS, setIsIOS] = useState(true)
+type Props = {
+  openChangeNodePopup: () => void
+}
 
-  const openChangeNodePopup = useCallback(() => dispatch(toggleRPCNodePopup(true)), [])
-  const closeModalHandler = useCallback(() => dispatch(toggleRPCNodePopup(false)), [])
-
-  useEffect(() => {
-    if (
-      localStorage.getItem('beacon:active-account') &&
-      localStorage.getItem('beacon:active-account') !== 'undefined'
-    ) {
-      dispatch(connect())
-    }
-
-    if (!getItemFromStorage('theme')) {
-      setItemInStorage('theme', SPACE_THEME)
-    }
-
-    setIsIOS(
-      ['iPad Simulator', 'iPhone Simulator', 'iPod Simulator', 'iPad', 'iPhone', 'iPod'].includes(navigator.platform),
-    )
-  }, [])
-  
-
-  if (isIOS && changeNodePopupOpen) {
-    return <PopupChangeNodeView closeModal={closeModalHandler} />
-  }
-
+export function Bakery ({ openChangeNodePopup }: Props) {
   return (
-    <SWRConfig
-      value={{
-        refreshInterval: 5000,
-        fetcher: dexGqlFetcher,
-      }}
-      >
-        <PopupChangeNode isModalOpened={!isIOS && changeNodePopupOpen} closeModal={closeModalHandler} />
-        <MenuTopBar openChangeNodePopupHandler={openChangeNodePopup} />
-        <BakeryView />
-        <FooterStyled />
-    </SWRConfig>
+    <>
+      <MenuTopBar openChangeNodePopupHandler={openChangeNodePopup} />
+      <BakeryView />
+      <FooterStyled />
+    </>
   )
 }
