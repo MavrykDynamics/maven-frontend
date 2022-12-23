efa207cdfc2ef29673f75a49741bec49458a797c
Evgeniy Polishchuk <68184643+CasualJackie@users.noreply.github.com>
2022-12-23 15:32:16 +0200

    rolled back RPCNodePopup to bakery

--- src/frontend/src/pages/Bakery/Bakery.controller.tsx
+++ src/frontend/src/pages/Bakery/Bakery.controller.tsx
@@ -1,17 +1,55 @@
+import { useCallback, useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+
 // components
+import { PopupChangeNodeView } from 'app/App.components/SettingsPopup/SettingsPopup.view'
+import { PopupChangeNode } from 'app/App.components/SettingsPopup/SettingsPopup.controller'
+import { connect } from 'redux/actions/connectWallet.actions'
 import { BakeryView } from './Bakery.view'
 import { MenuTopBar } from 'app/App.components/Menu/MenuTopBar.controller'
 
 // styles
 import { FooterStyled } from "./Bakery.style";
 
-type Props = {
-  openChangeNodePopup: () => void
-}
+// helpers
+import { SPACE_THEME, toggleRPCNodePopup } from 'redux/actions/preferences.action'
+import { getItemFromStorage, setItemInStorage } from 'utils/utils'
 
-export function Bakery ({ openChangeNodePopup }: Props) {
+// types
+import { State } from 'utils/interfaces'
+
+export function Bakery () {
+  const dispatch = useDispatch()
+  const { changeNodePopupOpen } = useSelector((state: State) => state.preferences)
+  const [isIOS, setIsIOS] = useState(true)
+
+  const openChangeNodePopup = useCallback(() => dispatch(toggleRPCNodePopup(true)), [])
+  const closeModalHandler = useCallback(() => dispatch(toggleRPCNodePopup(false)), [])
+
+  useEffect(() => {
+    if (
+      localStorage.getItem('beacon:active-account') &&
+      localStorage.getItem('beacon:active-account') !== 'undefined'
+    ) {
+      dispatch(connect())
+    }
+
+    if (!getItemFromStorage('theme')) {
+      setItemInStorage('theme', SPACE_THEME)
+    }
+
+    setIsIOS(
+      ['iPad Simulator', 'iPhone Simulator', 'iPod Simulator', 'iPad', 'iPhone', 'iPod'].includes(navigator.platform),
+    )
+  }, [])
+  
+
+  if (isIOS && changeNodePopupOpen) {
+    return <PopupChangeNodeView closeModal={closeModalHandler} />
+  }
   return (
     <>
+      <PopupChangeNode isModalOpened={!isIOS && changeNodePopupOpen} closeModal={closeModalHandler} />
       <MenuTopBar openChangeNodePopupHandler={openChangeNodePopup} />
       <BakeryView />
       <FooterStyled />
