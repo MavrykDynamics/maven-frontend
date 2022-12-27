5c3d27c81621e9a4b39d1420bde33f5a28cc4ffd
Evgeniy Polishchuk <68184643+CasualJackie@users.noreply.github.com>
2022-12-27 22:05:22 +0200

    added toaster and loader to delegation action

--- src/frontend/src/redux/actions/bakery.action.ts
+++ src/frontend/src/redux/actions/bakery.action.ts
@@ -1,6 +1,12 @@
 import type { AppDispatch, GetState } from '../../app/App.controller'
 import { State } from 'utils/interfaces'
 import { TezosToolkit } from '@taquito/taquito'
+import { showToaster } from '../../app/App.components/Toaster/Toaster.actions'
+import { ERROR, INFO, SUCCESS } from '../../app/App.components/Toaster/Toaster.constants'
+import { BeaconWallet } from '@taquito/beacon-wallet'
+import { checkIfWalletIsConnected, WalletOptions } from './connectWallet.actions'
+import { toggleLoader } from './preferences.action'
+import { ROCKET_LOADER } from 'utils/consts'
 
 export type BakeryDelegateDataType = {
   balance: number
@@ -15,13 +21,34 @@ export const getBakeryDelegateData = async (bakerAddress: string): Promise<Baker
 
 export const delegation = (bekerAddress: string) => async (dispatch: AppDispatch, getState: GetState) => {
   const state: State = getState()
+
+  if (!state.wallet.ready) {
+    dispatch(showToaster(ERROR, 'Please connect your wallet', 'Click Connect in the left menu'))
+    return
+  }
+
+  if (state.loading) {
+    dispatch(showToaster(ERROR, 'Cannot send transaction', 'Previous transaction still pending...'))
+    return
+  }
+
   try {
     const rpcNetwork = state.preferences.REACT_APP_RPC_PROVIDER || 'https://mainnet.smartpy.io'
-    const Tezos = new TezosToolkit(rpcNetwork)
-    
-    await Tezos.contract.setDelegate({ source: 'tz1_source', delegate: bekerAddress });
-    console.error(`Success delegation:`)
-  } catch (error) {
+    const wallet = new BeaconWallet(WalletOptions)
+    const walletResponse = await checkIfWalletIsConnected(wallet)
+
+    if (walletResponse) {
+      await dispatch(toggleLoader(ROCKET_LOADER))
+
+      const Tezos = new TezosToolkit(rpcNetwork)
+      await Tezos.contract.setDelegate({ source: 'tz1_source', delegate: bekerAddress });
+
+      await dispatch(toggleLoader())
+      await dispatch(showToaster(SUCCESS, 'Successful delegation', 'All good :)'))
+    }
+  } catch (error: any) {
     console.error(`Failed delegation:`, error)
+    dispatch(showToaster(ERROR, 'Error', error.message))
+    dispatch(toggleLoader())
   }
 }
