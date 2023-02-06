84efd1ab890c7061bac1bda2082226ce18f66ec2
Sam Morrow <smorrow32@gmail.com>
2023-02-06 17:57:16 +0200

    remove console log

--- src/frontend/src/pages/Litepaper/Litepaper.controller.tsx
+++ src/frontend/src/pages/Litepaper/Litepaper.controller.tsx
@@ -3,6 +3,5 @@ import * as React from 'react'
 import { LitepaperView } from './Litepaper.view'
 
 export const Litepaper = () => {
-  console.log('test cloudflare update')
   return <LitepaperView />
 }
