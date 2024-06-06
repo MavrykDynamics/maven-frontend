492ba83a899abf7c7bbf0e3fd71d7b4220df9f64
Sam Morrow <smorrow32@gmail.com>
2024-06-06 14:41:21 +0400

    change from ghostnet to atlasnet

--- src/frontend/src/utils/env.ts
+++ src/frontend/src/utils/env.ts
@@ -1,8 +1,8 @@
 const env = {
     // gqlLink: 'https://dex.dipdup.net/v1/graphql'
     gqlLink: 'https://api.mavenfinance.io/v1/graphql',
-    rpcLink: 'https://rpc.ghostnet.teztnets.xyz/',
-    rpcTestNetNetwork: 'ghostnet',
+    rpcLink: 'https://atlasnet.rpc.mavryk.network',
+    rpcTestNetNetwork: 'atlasnet',
     NODE_ENV: 'production',
     // rpcLink: 'https://mainnet.tezos.marigold.dev/',
 }
