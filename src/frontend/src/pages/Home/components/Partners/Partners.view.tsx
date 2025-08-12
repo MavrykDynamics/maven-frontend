62fb382280e11cacf3293a5b88c44c63ccc630ed
alexbelz123 <belz.alexx@gmail.com>
2025-08-12 16:46:09 +0300

    replaced tezos logo, commented out bakery IL

--- src/frontend/src/pages/Home/components/Partners/Partners.view.tsx
+++ src/frontend/src/pages/Home/components/Partners/Partners.view.tsx
@@ -112,18 +112,20 @@ export const PartnersView = () => {
           <img alt="partner" src="/images/blockchase.svg" />
         </a>
 
-        {/* <a className="empty" /> */}
         <a href="https://www.cluster.vc/" target="_blank" rel="noreferrer" className="partner-cluster">
           <div>
             <img alt="partner" src="/images/partners/cluster.png" />
           </div>
         </a>
+        <a className="empty" />
+
         <a href="https://neofacto.com/home/" target="_blank" rel="noreferrer" className="partner-neo-facto">
           <img alt="partner" src="/images/partners/neo-facto.svg" />
         </a>
         <a href="https://www.nomadic-labs.com/" target="_blank" rel="noreferrer" className="partner-nomadic">
           <img alt="partner" src="/images/partners/nomadic-labs.svg" />
         </a>
+        <a className="empty" />
       </PartnersGrid>
     </PartnersStyled>
   )
